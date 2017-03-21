5af1794691df2147b043eb4ea7e4bf79ed38ee22
Erik S Carlsten <ecarlste@gmail.com>
2017-03-21 17:03:10 -0500

    Added RoleService business service to wire the controller to the repository

---
+++ src/main/java/com/erikcarlsten/effectivehabits/roleservice/service/RoleService.java
@@ -0,0 +1,41 @@
+package com.erikcarlsten.effectivehabits.roleservice.service;
+
+import com.erikcarlsten.effectivehabits.roleservice.domain.Role;
+import com.erikcarlsten.effectivehabits.roleservice.domain.RoleRepository;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class RoleService {
+
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public Collection<Role> findAll() {
+        return Lists.newArrayList(roleRepository.findAll());
+    }
+
+    public Role findOne(long id) {
+        return roleRepository.findOne(id);
+    }
+
+    public Role create(Role role) {
+        return roleRepository.save(role);
+    }
+
+    public Role update(long id, Role role) {
+        return roleRepository.save(role);
+    }
+
+    public void delete(long id) {
+        roleRepository.delete(id);
+    }
+
+}
