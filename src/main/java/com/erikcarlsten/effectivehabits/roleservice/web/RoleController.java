394c919d43667817fc585905c15b8d0cc89e3814
Erik S Carlsten <ecarlste@gmail.com>
2017-03-21 18:39:42 -0500

    Added several methods to the RoleController to provide JSON responses
    - findAll
    - create
    - findOne
    
    Currently HATEOAS is enabled correctly for the findAll and findOne endpoints, but it is not working for some reason in the findOne method.

--- src/main/java/com/erikcarlsten/effectivehabits/roleservice/web/RoleController.java
+++ src/main/java/com/erikcarlsten/effectivehabits/roleservice/web/RoleController.java
@@ -1,15 +1,40 @@
 package com.erikcarlsten.effectivehabits.roleservice.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.erikcarlsten.effectivehabits.roleservice.domain.Role;
+import com.erikcarlsten.effectivehabits.roleservice.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/roles")
 public class RoleController {
 
-    @RequestMapping("/")
-    public String getAllRoles() {
-        return "All Roles";
+    RoleService roleService;
+
+    @Autowired
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Collection<Role>> getAllRoles() {
+        return new ResponseEntity<>(roleService.findAll(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Role> getRole(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(roleService.findOne(id), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Role> createRole(@RequestBody Role role) {
+        return new ResponseEntity<>(role, HttpStatus.CREATED);
     }
 
 }
