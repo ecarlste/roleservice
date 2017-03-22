20bbd06d2d6da76dddbdb7a20c99361c57b0666f
Erik S Carlsten <ecarlste@gmail.com>
2017-03-21 19:29:02 -0500

    Added several API methods to the RoleController
    - getRole
    - updateRole
    - deleteRole
    
    All CRUD requirements now met for this Controller.

--- src/main/java/com/erikcarlsten/effectivehabits/roleservice/web/RoleController.java
+++ src/main/java/com/erikcarlsten/effectivehabits/roleservice/web/RoleController.java
@@ -26,15 +26,30 @@ public class RoleController {
         return new ResponseEntity<>(roleService.findAll(), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Role> getRole(@PathVariable("id") Long id) {
-        return new ResponseEntity<>(roleService.findOne(id), HttpStatus.OK);
-    }
-
     @RequestMapping(value = "/", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Role> createRole(@RequestBody Role role) {
         return new ResponseEntity<>(role, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Role> getRole(@PathVariable("id") Long id) {
+        Role role = roleService.findOne(id);
+        HttpStatus httpStatus = role == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
+        return new ResponseEntity<>(role, httpStatus);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Role> updateRole(@PathVariable("id") Long id, @RequestBody Role role) {
+        Role updatedRole = roleService.update(id, role);
+        HttpStatus httpStatus = updatedRole == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
+        return new ResponseEntity<>(updatedRole, httpStatus);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Role> deleteRole(@PathVariable("id") Long id) {
+        return new ResponseEntity<Role>(HttpStatus.OK);
     }
 
 }
