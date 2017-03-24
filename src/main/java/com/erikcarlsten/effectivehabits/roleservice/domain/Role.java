2e4979251575707e0ce8f01bb2da74ab7a89967f
Erik S Carlsten <ecarlste@gmail.com>
2017-03-23 20:05:35 -0500

    Changed access to the no argument constructor to private

--- src/main/java/com/erikcarlsten/effectivehabits/roleservice/domain/Role.java
+++ src/main/java/com/erikcarlsten/effectivehabits/roleservice/domain/Role.java
@@ -5,21 +5,20 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Role {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     private long id;
 
     @Getter
     @Setter
     private String name;
 
-    public Role() {}
+    private Role() {}
 
     public Role(String name) {
         this.name = name;
