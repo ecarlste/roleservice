f85498437185e9f3ba1eeb6e778e174600360607
Erik S Carlsten <ecarlste@gmail.com>
2017-03-23 20:07:00 -0500

    Added custom not found exception to the RoleController

---
+++ src/main/java/com/erikcarlsten/effectivehabits/roleservice/web/ExceptionControllerAdvice.java
@@ -0,0 +1,7 @@
+package com.erikcarlsten.effectivehabits.roleservice.web;
+
+/**
+ * Created by ecarlste on 3/23/17.
+ */
+public class ExceptionControllerAdvice {
+}
--- src/main/java/com/erikcarlsten/effectivehabits/roleservice/web/RoleController.java
+++ src/main/java/com/erikcarlsten/effectivehabits/roleservice/web/RoleController.java
@@ -1,7 +1,10 @@
 package com.erikcarlsten.effectivehabits.roleservice.web;
 
 import com.erikcarlsten.effectivehabits.roleservice.domain.Role;
+import com.erikcarlsten.effectivehabits.roleservice.exception.RoleNotFoundException;
 import com.erikcarlsten.effectivehabits.roleservice.service.RoleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,7 +17,9 @@ import java.util.Collection;
 @RequestMapping("/roles")
 public class RoleController {
 
-    RoleService roleService;
+    private static final Logger logger = LoggerFactory.getLogger(RoleController.class);
+
+    private RoleService roleService;
 
     @Autowired
     public RoleController(RoleService roleService) {
@@ -29,27 +34,34 @@ public class RoleController {
     @RequestMapping(value = "/", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Role> createRole(@RequestBody Role role) {
-        return new ResponseEntity<>(role, HttpStatus.CREATED);
+        Role createdRole = roleService.create(role);
+        return new ResponseEntity<>(createdRole, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Role> getRole(@PathVariable("id") Long id) {
+    public ResponseEntity<Role> getRole(@PathVariable("id") long id) throws RoleNotFoundException {
         Role role = roleService.findOne(id);
-        HttpStatus httpStatus = role == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
-        return new ResponseEntity<>(role, httpStatus);
+
+        if (role == null) {
+            logger.info("Role with id: {} not found.", id);
+            throw new RoleNotFoundException("Role with id: " + id + " not found.");
+        }
+
+        return new ResponseEntity<>(role, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Role> updateRole(@PathVariable("id") Long id, @RequestBody Role role) {
+    public ResponseEntity<Role> updateRole(@PathVariable("id") long id, @RequestBody Role role) {
         Role updatedRole = roleService.update(id, role);
         HttpStatus httpStatus = updatedRole == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
         return new ResponseEntity<>(updatedRole, httpStatus);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Role> deleteRole(@PathVariable("id") Long id) {
-        return new ResponseEntity<Role>(HttpStatus.OK);
+    public ResponseEntity<Role> deleteRole(@PathVariable("id") long id) {
+        roleService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
