55aaf4b58825f2bb2442f9027b8aed9461f1cfba
Erik S Carlsten <ecarlste@gmail.com>
2017-03-23 20:12:15 -0500

    Added an exception handler to the generic controller advice

--- src/main/java/com/erikcarlsten/effectivehabits/roleservice/web/ExceptionControllerAdvice.java
+++ src/main/java/com/erikcarlsten/effectivehabits/roleservice/web/ExceptionControllerAdvice.java
@@ -1,7 +1,19 @@
 package com.erikcarlsten.effectivehabits.roleservice.web;
 
-/**
- * Created by ecarlste on 3/23/17.
- */
+import com.erikcarlsten.effectivehabits.roleservice.exception.RoleNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@ControllerAdvice
 public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(RoleNotFoundException.class)
+    public void handleRoleNotFound(RoleNotFoundException exception, HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+    }
+
 }
